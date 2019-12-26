c7f50208f91580131b4455056aaec2e8ae5e798e
Ganesh Khillari <33245448+ganeshkhillari@users.noreply.github.com>
2019-12-26 21:01:30 +0530

    first push

---
+++ registration_form.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+You have entered below details
+<br>
+First Name : <?php echo $_POST["firstname"]; ?><br>
+Last Name : <?php echo $_POST["lastname"]; ?><br>
+Email : <?php echo $_POST["email"]; ?><br>
+Mobile Number :<?php echo $_POST["mobile"]; ?><br>
+
+</body>
+</html>
