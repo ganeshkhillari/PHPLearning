2b78b6724f9afcdde555efd0d664a3c4e9285955
Ganesh Khillari <33245448+ganeshkhillari@users.noreply.github.com>
2019-12-29 17:00:24 +0530

    admin pannel added

---
+++ admin.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "localhost";
+$username = "u331594503_ganeshkhillari";
+$password = "admin";
+$dbname = "u331594503_users";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn)
+{
+  echo "connection failed";
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM USER_DETAILS";
+$res = $conn->query($sql);
+if ($res->num_rows > 0)
+{
+	echo "<table>";
+  echo "<tr>";
+  echo "<th>First Name</th>";
+  echo "<th>Last Name</th>";
+  echo "<th>EMAIL</th>";
+	echo "<th>Mobile number</th>"
+  echo "</tr>";
+  while($row = $result->fetch_assoc())
+	{
+  	echo "<tr>";
+    echo "<td>".$row['F_NAME']."</td>";
+    echo "<td>".$row['L_NAME']."</td>";
+    echo "<td>".$row['EMAIL']."</td>";
+		echo "<td>".$row['MB_NO']."</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+  mysqli_free_res($res);
+}
+else
+{
+  echo "No matching records are found.";
+}
+?>
--- index.php
+++ index.php
@@ -191,34 +191,7 @@ input[type=submit]:hover {
 				{
     			echo "Error: " . $sql . "<br>" . $conn->error;
 				}
-echo "start";
-				$sql = "SELECT * FROM USER_DETAILS";
-				$res = $conn->query($sql);
-				if ($res->num_rows > 0)
-				{
-					echo "<table>";
-				  echo "<tr>";
-				  echo "<th>First Name</th>";
-				  echo "<th>Last Name</th>";
-				  echo "<th>EMAIL</th>";
-					echo "<th>Mobile number</th>"
-				  echo "</tr>";
-				  while($row = $result->fetch_assoc())
-					{
-				  	echo "<tr>";
-				    echo "<td>".$row['F_NAME']."</td>";
-				    echo "<td>".$row['L_NAME']."</td>";
-				    echo "<td>".$row['EMAIL']."</td>";
-						echo "<td>".$row['MB_NO']."</td>";
-				    echo "</tr>";
-				  }
-				  echo "</table>";
-				  mysqli_free_res($res);
-				}
-				else
-				{
-					echo "No matching records are found.";
-				}
+				
 				$conn->close();
 			}
 		?>
